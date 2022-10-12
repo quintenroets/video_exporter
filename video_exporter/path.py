1d618d5032c875927bb03088793c6b5fae2bc23e
Quinten <quinten.roets@gmail.com>
2022-10-12 16:58:21 -0400

    make package ready for nondev install

--- video_exporter/path.py
+++ video_exporter/path.py
@@ -4,6 +4,6 @@ root = plib.Path(__file__).parent
 
 
 class Path(plib.Path):
-    templates = root.parent / "assets" / "templates"
+    templates = root / "assets" / "templates"
     template1 = templates / "video.html"
     template2 = templates / "video2.html"
