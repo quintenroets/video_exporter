e0383b4347cbf0446df89f7ec82567f9a2538086
Quinten <quinten.roets@gmail.com>
2023-02-23 16:26:48 -0500

    publish on pypi

--- setup.py
+++ setup.py
@@ -1,25 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-NAME = "video_exporter"
-
-
-def read(filename):
-    try:
-        with open(filename) as fp:
-            content = fp.read().split("\n")
-    except FileNotFoundError:
-        content = []
-    return content
-
-
-setup(
-    author="Quinten Roets",
-    author_email="quinten.roets@gmail.com",
-    description="",
-    name=NAME,
-    version="1.0",
-    packages=find_packages(),
-    package_data={"video_exporter": ["assets/**"]},
-    install_requires=read("requirements.txt"),
-    entry_points={"console_scripts": ["export_videos = video_exporter.main:main"]},
-)
+if __name__ == "__main__":
+    setup()
